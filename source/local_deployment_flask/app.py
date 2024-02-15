455c0c689e3600c66eea687ac420c3281eb43150
Ard Snijders <ardsnijders@gmail.com>
2024-02-15 10:24:12 +0100

    Fix import error, fix wrong filepaths, fix faulty model load

--- source/local_deployment_flask/app.py
+++ source/local_deployment_flask/app.py
@@ -1,16 +1,9 @@
 from flask import Flask, render_template, request, send_from_directory
 import pickle
 import os
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import requests 
-import cv2 
-import imutils 
 from ultralytics import YOLO
-import base64
 import numpy as np
-import io
 from PIL import Image
 
 import warnings
@@ -20,7 +13,10 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 app = Flask(__name__)
 
 # Load pickled YOLO model
-model = pickle.load(open('model.pkl', 'rb'))
+# if os.path.isfile('model.pkl'):
+#     model = pickle.load(open('model.pkl', 'rb'))
+# else:
+model = YOLO('yolov8n.pt')
 
 # Set homepage for web-app. Prompts Flask app to run index.html as found under .\templates at localhost:5000
 # This page provides functionality for user to upload an image file and send it through an HTTP POST request to the Flask app.
@@ -43,7 +39,8 @@ def predict():
     im_array = results[0].plot() # extract prediction from YOLO results object
     im = Image.fromarray(im_array[..., ::-1])  # load result array into PIL again
 
-    f_path = 'dynamic/results.jpg'
+    print(os.getcwd())
+    f_path = 'source/local_deployment_flask/dynamic/results.jpg'
     im.save(f_path)  # save image to local folder
     return send_from_directory('dynamic', 'results.jpg') # serve image with added bounding boxes back to user
     # return render_template('image_render.html', image=f_path)
